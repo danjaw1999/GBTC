8a93d229c2f8d86216e8ff5a7b8940a1377ce30d
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-10-17 12:24:08 +0200

    Updates

---
+++ precache-manifest.42681da886c0027e66a1ba5729a599b7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3ee19ba0493bb80cddaf99088e17e2",
+    "url": "/Oddaj-Rzeczy/index.html"
+  },
+  {
+    "revision": "40af5622b3e3b63158a0",
+    "url": "/Oddaj-Rzeczy/static/css/main.0f108e45.chunk.css"
+  },
+  {
+    "revision": "c77640534e35dab52390",
+    "url": "/Oddaj-Rzeczy/static/js/2.aaab2335.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Oddaj-Rzeczy/static/js/2.aaab2335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40af5622b3e3b63158a0",
+    "url": "/Oddaj-Rzeczy/static/js/main.1157fd1d.chunk.js"
+  },
+  {
+    "revision": "12eb0336a086ddddae6a",
+    "url": "/Oddaj-Rzeczy/static/js/runtime-main.322ac6a6.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Oddaj-Rzeczy/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Oddaj-Rzeczy/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Oddaj-Rzeczy/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Oddaj-Rzeczy/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Oddaj-Rzeczy/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Oddaj-Rzeczy/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Oddaj-Rzeczy/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/Oddaj-Rzeczy/static/media/homeThreeColumns.99f2e620.png"
+  }
+]);
