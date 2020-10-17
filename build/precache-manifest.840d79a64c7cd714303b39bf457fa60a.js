67791e3769151607611258c909a6f9e247cc35eb
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-10-17 12:18:46 +0200

    Deploy

---
+++ build/precache-manifest.840d79a64c7cd714303b39bf457fa60a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9149dec7b8ec9aa6fe264c7f59c51012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef84d9f9758fe2fc5a6c",
+    "url": "/static/css/main.39e5b7eb.chunk.css"
+  },
+  {
+    "revision": "c77640534e35dab52390",
+    "url": "/static/js/2.aaab2335.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aaab2335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef84d9f9758fe2fc5a6c",
+    "url": "/static/js/main.beed0e76.chunk.js"
+  },
+  {
+    "revision": "d1ddbd4df83493372bdb",
+    "url": "/static/js/runtime-main.72f44b10.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/static/media/homeThreeColumns.99f2e620.png"
+  }
+]);
